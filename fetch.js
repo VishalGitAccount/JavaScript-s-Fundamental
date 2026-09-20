6dfda0cd974ca259ba855ae2dc27e4b2663a6afa
unknown <vishal.easystudies@gmail.com>
2026-09-20 21:21:58 +0530

    Promises Work

--- fetch.js
+++ fetch.js
@@ -29,25 +29,45 @@
 //     console.log(data)
 // }).catch((error) => {console.log(error)})
 
-var URL = "https://v2.jokeapi.dev/joke/Programming?type=single";
+// var URL = "https://v2.jokeapi.dev/joke/Programming?type=single";
 
-function sendRequest(URL) {
-    return new Promise((resolve, reject) => {
-        var request = new XMLHttpRequest();
-        request.addEventListener("readystatechange", () => {
-            if (request.readyState === 4 && request.status === 200) {
-                resolve(JSON.parse(request.responseText).joke);
-            }
-            if (request.readyState === 4 && request.status !== 200) {
-                reject("Failed to fetch the joke");
-            }
-        });
-        request.open("GET", URL);
-        request.send();
-    })
-}
-sendRequest(URL).then((data) => {
-    console.log(data)
-}).catch((error) => {
-    console.log(error)
-})
+// function sendRequest(URL) {
+//     return new Promise((resolve, reject) => {
+//         var request = new XMLHttpRequest();
+//         request.addEventListener("readystatechange", () => {
+//             if (request.readyState === 4 && request.status === 200) {
+//                 resolve(JSON.parse(request.responseText).joke);
+//             }
+//             if (request.readyState === 4 && request.status !== 200) {
+//                 reject("Failed to fetch the joke");
+//             }
+//         });
+//         request.open("GET", URL);
+//         request.send();
+//     })
+// }
+// Method -1 handle the promises through the then and catch this is older concept to follow.
+// sendRequest(URL).then((data) => {
+//     console.log(data)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+// async function handleRequest(URL) {
+//     try {
+//         var data = await sendRequest(URL);
+//         console.log(data);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+// handleRequest(URL);
+
+
+// Promise.all([sendRequest(URL), sendRequest(URL)]).then(([r1, r2]) => {
+//     console.log("J1", r1, "\nJ2", r2)
+// }).catch((error) => {
+//     console.log(error)
+// });
+
+
